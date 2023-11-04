8e4416401706892f38e5d700fff373b378305ec2
Philippe Nikolov <philippe.nikolov@outlook.com>
2023-11-04 13:19:34 -0400

    added fluid

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,13 +1,30 @@
-function setup() {
-  // put setup code here
-  createCanvas(1200, 700);
-  frameRate(60);
+let fluid;
 
+function setup() {
+  createCanvas(1200, 650);
+  frameRate(60);
+  fluid = new Fluid(0.2, 0, 0.0000001);
 }
 
 function draw() {
-  // put drawing code here
-  // draw rectangle
-  background(0);
-  fill(255);
+  stroke(51);
+  strokeWeight(2);
+
+  let cx = int((0.5 * width) / SCALE);
+  let cy = int((0.5 * height) / SCALE);
+  for (let i = -1; i <= 1; i++) {
+    for (let j = -1; j <= 1; j++) {
+      fluid.addDensity(cx + i, cy + j, random(50, 150));
+    }
+  }
+
+  for (let i = 0; i < 2; i++) {
+    let angle = noise(t) * TWO_PI * 2;
+    let v = p5.Vector.fromAngle(angle);
+    v.mult(0.2);
+    t += 0.01;
+   fluid.addVelocity(cx, cy, v.x, v.y);
+  }
+  fluid.step();
+  fluid.renderD();
 }
