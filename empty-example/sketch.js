0b2dae5b5f1e8b12fad0539ad2881ec31225eccd
Philippe Nikolov <philippe.nikolov@outlook.com>
2023-11-04 11:08:53 -0400

    track js and html

---
+++ empty-example/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  // put setup code here
+  createCanvas(1200, 700);
+  frameRate(60);
+
+}
+
+function draw() {
+  // put drawing code here
+  // draw rectangle
+  background(0);
+  fill(255);
+}
