bf90d6e3e1b9cf6939bbba2c9169f75df8a91633
Philippe Nikolov <philippe.nikolov@outlook.com>
2023-11-05 02:03:25 -0500

    front-end stuff

--- empty-example/handleStates.js
+++ empty-example/handleStates.js
@@ -4,28 +4,46 @@ let modes = {
     ffa: 2
 };
 
-let gamemode = modes.singleplayer;
+let gamemode;
 
 //Declaration of all buttons for changing gamemodes
 const singleplayerButton = document.getElementById('singleplayer-btn');
 const multiplayerButton = document.getElementById('multiplayer-btn');
 const ffaButton = document.getElementById('ffa-btn');
 
+const buttons = [singleplayerButton, multiplayerButton, ffaButton]; // Array of buttons
 
 const changeGamemode = (mode) => {
     reset();
     gamemode = mode;
     setup();
+
+    // Remove active class from all buttons
+    buttons.forEach(button => {
+        button.classList.remove('active-game-mode');
+    });
+
+    // Add active class to the clicked button
+    const activeButton = buttons.find(button => parseInt(button.value) === mode);
+    if (activeButton) {
+        activeButton.classList.add('active-game-mode');
+    }
 }
 
+// Event listeners for buttons
 singleplayerButton.addEventListener('click', () => {
     changeGamemode(modes.singleplayer);
+    singleplayerButton.classList.add('active-game-mode');
 });
 
 multiplayerButton.addEventListener('click', () => {
-    changeGamemode(modes.multiplayer); 
+    changeGamemode(modes.multiplayer);
+    multiplayerButton.classList.add('active-game-mode');
 });
 
 ffaButton.addEventListener('click', () => {
     changeGamemode(modes.ffa);
+    ffaButton.classList.add('active-game-mode');
 });
+
+changeGamemode(modes.singleplayer);
